b9a772fc51a217239c3499b70b19d745cd687926
Eldon <Birdheldon@gmail.com>
2025-08-02 16:36:23 -0600

    initial

---
+++ js/api.js
@@ -0,0 +1,19 @@
+﻿const apiurl = "https://kiki-server.net/api/";
+
+
+function retreive_module(Id){
+
+  var result = "Whatever is the module data?"
+
+
+  return result;
+}
+
+
+
+function testing_module_get(Module_id){
+
+
+
+  return [Module_id, "Dallas", [37.7969462,-122.4313725], 20.5, 80, 50, "12:00-01/01/25", 24, 18];
+}
---
+++ js/script_home.js
@@ -0,0 +1,9 @@
+﻿var map = L.map('map').setView([32.8189166667, -98.1016666667], 13);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([32.8189166667, -98.1016666667]).addTo(map);
